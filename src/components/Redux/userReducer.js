7767284a73e0d0298e0a6419bd7df907ffb0700a
Dtsoy <dmitriytsoy123@gmail.com>
2023-03-09 15:15:26 -0800

    pages

--- src/components/Redux/userReducer.js
+++ src/components/Redux/userReducer.js
@@ -1,17 +1,14 @@
-
-const FOLLOW =  'FOLLOW';
+const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
-let initialState =  {
-  users : [
-      {id: 1, photoUrl:'https://sm.ign.com/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.jpg', followed:false, fullName: 'Dmitriy', status: "holla!", location: {city:"Los Angeles", country: "USA"}},
-      {id: 2, photoUrl:'https://cms.bitent.com/assets/games/thumbs/mega_anime_avatar_creator_630e38ccc00ad.png', followed:true, fullName: 'Oleg', status: "Living my perfect life!", location: {city:"Los Angeles", country: "USA"}},
-      {id: 3, photoUrl:'https://www.pngkey.com/png/detail/572-5723307_kaneki-ken-kanekiken-kaneki-anime.png',followed:false, fullName: 'Petr', status: "Just you and me", location: {city:"Los Angeles", country: "USA"}},
-      {id: 4, photoUrl:'https://img.faceyourmanga.com/mangatars/2/78/2078772/large_1882160.png',followed:true, fullName: 'Domo', status: "Became insane!", location: {city:"Los Angeles", country: "USA"}},
-      {id: 5, photoUrl:'https://pub-static.fotor.com/assets/projects/pages/e174890b7d614925a4f275b67873ffb7/300w/fotor-01b0203e5e414695890876d329e551d3.jpg',followed:false, fullName: 'Hon See', status: "Bam bam MF", location: {city:"Los Angeles", country: "USA"}},
-      {id: 6, photoUrl:'https://pub-static.fotor.com/assets/projects/pages/1bd9e5e88594483ba733ca32245f007f/300w/fotor-39237a95a12545389fcfaf11a1e97c69.jpg',followed:false, fullName: 'Dapindar', status: "deus ex machina", location: {city:"Los Angeles", country: "USA"}},
-      ]
+let initialState = {
+    users: [ ],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 1
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -37,14 +34,24 @@ const usersReducer = (state = initialState, action) => {
                 })
             }
         case SET_USERS: {
-            return { ...state, users: [ ...state.users, ...action.users ]}
+            return { ...state, users: action.users }
+        }
+        case SET_CURRENT_PAGE: {
+            return { ...state, currentPage: action.currentPage}
+        }
+        case SET_TOTAL_USERS_COUNT: {
+            return { ...state, totalUsersCount: action.count}
         }
         default:
             return state;
-    };
-};
+    }
+}
 
-export const followAC = (userId) => ({type: FOLLOW, userId});
-export const unFollowAC = (userId) => ({type: UNFOLLOW, userId});
-export const setUsers = (userId) => ({type: SET_USERS, userId});
+
+export const followAC = (userId) => ({type: FOLLOW, userId })
+export const unfollowAC = (userId) => ({type: UNFOLLOW, userId })
+export const setUsersAC = (users) => ({type: SET_USERS, users })
+export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage })
+export const setUsersTotalCountAC = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalUsersCount })
+
 export default usersReducer;
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,31 +1,39 @@
-import React from "react";
-import styles from "./Users.module.css"
-import axios from "axios";
+import React from 'react';
+import styles from "./Users.module.css";
+import userPhoto from "./userPhoto.png";
 
+let Users = (props) => {
 
-class Users extends React.Component{
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
-    componentDidMount(){
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(res => {this.props.setUsers(res.data.items);}) 
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
 
-    render(){
-        return <div>
-    
+    return <div>
+        <div>
+            {pages.map(p => {
+                return <span className={props.currentPage === p && styles.selectedPage}
+                             onClick={(e) => {
+                                 props.onPageChanged(p);
+                             }}>{p}</span>
+            })}
+        </div>
         {
-            this.props.users.map(u => <div key={u.id}>
+            props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={"https://cms.bitent.com/assets/games/thumbs/mega_anime_avatar_creator_630e38ccc00ad.png"} className={styles.userPhoto}/>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto}
+                             className={styles.userPhoto}/>
                     </div>
                     <div>
                         {u.followed
                             ? <button onClick={() => {
-                                this.props.unfollow(u.id)
+                                props.unfollow(u.id)
                             }}>Unfollow</button>
                             : <button onClick={() => {
-                                this.props.follow(u.id)
+                                props.follow(u.id)
                             }}>Follow</button>}
 
                     </div>
@@ -36,15 +44,13 @@ class Users extends React.Component{
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        {<div>{u.location.country}</div>}
-                        {<div>{u.location.city}</div>}
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
             </div>)
         }
     </div>
-    }
 }
-
 
 export default Users;
