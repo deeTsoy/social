3f4d920e6a00c7d77c9b85c0f558389ccc883091
Dtsoy <dmitriytsoy123@gmail.com>
2023-05-23 16:02:20 -0700

    textAreaValedation

--- src/components/Messages/NewMessageInput.jsx
+++ src/components/Messages/NewMessageInput.jsx
@@ -9,7 +9,11 @@ const NewMessageInput = (props) => {
     return (
         <form onSubmit={handleSubmit(props.onSubmit)}>
         <div>
-            <textarea name="newMessageData" placeholder={"Enter your message"} {...register('newMessageData')} />
+            <textarea name="newMessageData" placeholder={"Enter your message"} {...register('newMessageData', 
+            {   required: true,
+                maxLength: 30,
+                minLength: 5
+            })} />
         </div>
         <button>Submit</button>
         </form>
--- src/components/Profile/MyPosts/NewPostInput.jsx
+++ src/components/Profile/MyPosts/NewPostInput.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
+
 const NewPostInput = (props) => {
+
 
     const { register, handleSubmit } = useForm();
 
@@ -9,7 +11,11 @@ const NewPostInput = (props) => {
     return (
         <form onSubmit={handleSubmit(props.onSubmit)}>
         <div>
-            <textarea name="newPostData" placeholder={"Enter your message"} {...register('newPostData')} />
+            <textarea name="newPostData" placeholder={"Enter your message"} {...register('newPostData',
+            {   required: true,
+                maxLength: 30,
+                minLength: 5
+            })} />
         </div>
         <button>Add post</button>
         </form>
