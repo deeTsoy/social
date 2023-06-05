49ca04077b8573206912f28aac18de3db1f713d8
Dtsoy <dmitriytsoy123@gmail.com>
2023-06-05 14:32:27 -0700

    appReducer

--- src/components/Redux/reduxStore.jsx
+++ src/components/Redux/reduxStore.jsx
@@ -3,13 +3,15 @@ import profileReducer from "./profileReducer";
 import messageReducer from "./messageReducer";
 import usersReducer from "./userReducer";
 import authReducer from "./authReducer"
+import appReducer from "./appReducer"
 import thunk from "redux-thunk";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     messagesPage: messageReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    app: appReducer
 
 });
 
