76a1d92bdb0bdbd1ff7b4a915674c508a657f528
Dtsoy <dmitriytsoy123@gmail.com>
2023-06-05 16:47:20 -0700

    redirectUpdate

--- src/components/HOC/withAuthRedirect.jsx
+++ src/components/HOC/withAuthRedirect.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const withAuthRedirect = (Component) => {
   const RedirectComponent = (props) => {
     const isAuth = useSelector((state) => state.auth.isAuth);
-    const navigate = useNavigate();
+    let location = useLocation();
+
 
     if (!isAuth) {
-      navigate("/login");
-      return null; // Render nothing while redirecting
+      return <Navigate to="/login" state={{ from: location }} replace />;
+    }else{
+      return <Component {...props} />;
     }
-
-    return <Component {...props} />;
   };
 
   return RedirectComponent;
