a6d964c3166d5d3ffb0b1dfb79d4d9a0879e6d4a
Dtsoy <dmitriytsoy123@gmail.com>
2023-06-12 16:10:39 -0700

    profilPostUseMemo

--- src/components/Profile/MyPosts/Myposts.jsx
+++ src/components/Profile/MyPosts/Myposts.jsx
@@ -1,17 +1,18 @@
-import React from 'react'; 
+import React, { useMemo } from 'react'; 
 import Post from './post/post';
 import s from './MyPosts.module.css';
 import NewPostInput from './NewPostInput'
 
-const MyPosts = (props) => { 
+const MyPosts = ({addPost, profilePage}) => { 
   
-let posts = props.profilePage.postsData.map(item =>  item = <Post massage = {item.post} key={item.id}  likes = {item.like}/>)
-
-
+  let posts = useMemo(() => {
+     return  profilePage.postsData.map(item =>  item = <Post massage = {item.post} key={item.id}  likes = {item.like}/>)
+  }, [profilePage.postsData] )
 
 let addNewPost = (value) => {
-  props.addPost(value.newPostData)
+  addPost(value.newPostData)
 }
+console.log('ref')
 return (
       <div className={s.postsBlock}> 
         <h3> My posts </h3>
