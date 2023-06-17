0c90757592864b6af5018042e6ed8a3d8d905723
Dtsoy <dmitriytsoy123@gmail.com>
2023-06-16 23:01:18 -0700

    pageLine

--- src/components/Users/PagesLine.jsx
+++ src/components/Users/PagesLine.jsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from "./Users.module.css";
 
-const PagesLine = ({ currentPage, onPageChanged }) => {
+const PagesLine = ({ currentPage, onPageChanged,totalUsersCount, pageSize }) => {
     let pages = [];
     for (let i = 1; i <= 25; i++) {
       pages.push(i);
     }
+
+    let pagesCount =  Math.ceil(totalUsersCount / pageSize );
+    let portionCount = Math.ceil(pagesCount / 15);
+    let [portionNumber, setPortionNumber] = useState(1);
+    let leftPortionPageNumber = (portionNumber - 1) * 15 + 1;
+    let rightPortionPageNumber = portionNumber * 15;
   
-    return (
-      <div className={styles.middleLine}>
-        {pages.map(p => (
-          <button
-            className={currentPage === p ? styles.selectedPage : ''}
-            onClick={() => onPageChanged(p)}
-            key={p}
-          >
-            {p}
-          </button>
-        ))}
-      </div>
-    );
+
+    return <div className={styles.paginator}>
+        { portionNumber > 1 &&
+        <button onClick={() => { setPortionNumber(portionNumber - 1) }}>PREV</button> }
+
+            {pages
+                .filter(p => p >= leftPortionPageNumber && p<=rightPortionPageNumber)
+                .map((p) => {
+                return <button className={({
+                    [styles.selectedPage]: currentPage === p
+                }, styles.pageNumber) }
+                             key={p}
+                             onClick={(e) => {
+                                 onPageChanged(p);
+                             }}>{p}</button>
+            })}
+        { portionCount > portionNumber &&
+            <button onClick={() => { setPortionNumber(portionNumber + 1) }}>NEXT</button> }
+    </div>
   };
   
   export default PagesLine;
